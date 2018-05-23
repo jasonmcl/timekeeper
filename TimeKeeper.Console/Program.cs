90d43b37d039be1eef0ca1ed2355a23508635cd8
Jason McLaughlin <jasmclaughlin13@gmail.com>
2018-05-23 14:11:35 -0700

    Initial project creation

---
+++ TimeKeeper.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeKeeper.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+
+
+            System.Console.Read();
+        }
+    }
+}
